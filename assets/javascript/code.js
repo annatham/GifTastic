afd1b634655ee50ea495bc1c84c940a9b7013407
Anna Thammakane <anna.thammakane@gmail.com>
2019-10-03 07:04:32 -0500

    updated comments

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,25 +1,17 @@
 
 // var apiKey = "mvVlsyLAqUJwQYasHF5duopXShyoSo3d";
 
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET"
-  // }).then(function(response) {
-  //   console.log(queryURL);
-  //   console.log(response);
-  // });
-
-  // code to append data to page
 
   var searchResults = ["kittens", "puppies", "funny", "sad", "happy dance"];
 
-  // displayMovieInfo function re-renders the HTML to display the appropriate content
+  // displaySearch function renders the HTML to display content
   function displaySearch() {
 
     var searchID = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=mvVlsyLAqUJwQYasHF5duopXShyoSo3d&tag=" + searchID;
 
-    // Creating an AJAX call for the specific movie button being clicked
+    // Creating an AJAX call
+
     $.ajax({
       url: queryURL,
       method: "GET"
@@ -37,51 +29,50 @@
       // Appending the image
       searchDiv.append(image);
 
-      // Putting the entire movie above the previous movies
+      // adding image above last image
       $("#search-view").prepend(searchDiv);
     });
 
   }
 
-  // Function for displaying movie data
+  // Function for displaying buttons
   function renderButtons() {
 
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
+    // necessary to not have repeat buttons
     $("#search-history").empty();
 
-    // Looping through the array of movies
+    // Looping through the array
     for (var i = 0; i < searchResults.length; i++) {
 
-      // Then dynamicaly generating buttons for each movie in the array
-      /* This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>) */
+      // creating result button
       var a = $("<button>");
-      // Adding a class of movie-btn to our button
+
       a.addClass("search-btn");
-      // Adding a data-attribute
+      
       a.attr("data-name", searchResults[i]);
-      // Providing the initial button text
+
+      // give button text
       a.text(searchResults[i]);
-      // Adding the button to the buttons-view div
+
+      // Adding the button to the list
       $("#search-history").append(a);
     }
   }
-giovie button is clicked
+
+  // search results is rendered into button
   $("#add-search").on("click", function(event) {
     event.preventDefault();
     // This line grabs the input from the textbox
     var searchID = $("#search-form").val().trim();
 
-    // Adding movie from the textbox to our array
+    // Adding to array
     searchResults.push(searchID);
 
-    // Calling renderButtons which handles the processing of our movie array
     renderButtons();
   });
 
 
-  // Adding a click event listener to all elements with a class of "movie-btn"
+  //click event
   $(document).on("click", ".search-btn", displaySearch);
 
-  // Calling the renderButtons function to display the intial buttons
   renderButtons();
